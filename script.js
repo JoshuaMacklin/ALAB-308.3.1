565d75ee3e9b5eda612a35510d4c1d76cd077821
JoshuaMacklin <Joshua.macklin@pm.me>
2024-09-16 01:50:35 -0700

    Update script.js

--- script.js
+++ script.js
@@ -82,10 +82,32 @@ for (i=0; i <= 100; i++){
 // Index,Mass (kg),Spring 1 (m),Spring 2 (m)\n1,0.00,0.050,0.050\n2,0.49,0.066,0.066\n3,0.98,0.087,0.080\n4,1.47,0.116,0.108\n5,1.96,0.142,0.138\n6,2.45,0.166,0.158\n7,2.94,0.193,0.174\n8,3.43,0.204,0.192\n9,3.92,0.226,0.205\n10,4.41,0.238,0.232
 
 n = "ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n63,Blaine,Quiz Master,58\n98,Bill,Doctor’s Assistant,26"
+// nsplit = n.split(/\r?\n/)
+let cell1 = ''
+let cell2 = ''
+let cell3 = ''
+let cell4 = ''
+let cells = []
+numSinceLastCell = 0
+
+console.log(nsplit)
+
+// console.log(n.slice(0,2))
+
 for(let i=0; i <= n.length; i++) {
-  if (n.i == `,`){
-    console.log("FOUND A BREAK")
+  debugger
+  if ((n[i]+n[i+1]) != "\n") {
+    if (n[i] == `,`){
+      debugger
+      console.log(n.slice(numSinceLastCell,i))
+      // cells.push(n.slice(n,i))
+      numSinceLastCell = i + 1
+    } else {
+      debugger
+      console.log(`${n[i]} : ${numSinceLastCell}`);
+      // numSinceLastCell++;
+    }
   } else {
-    console.log(n.i)
+    console.log("new row")
   }
 }
